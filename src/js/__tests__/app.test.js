4cc321cb0113ffeb2c2377ebe777c0845430db84
Alexander Burachkov <zicio@yandex.ru>
2021-07-07 23:40:46 +0400

    Updated

--- src/js/__tests__/app.test.js
+++ src/js/__tests__/app.test.js
@@ -1,11 +1,11 @@
 import * as classes from '../app';
-import defaultClass from '../app';
+import Character from '../app';
 
 test('class Bowerman', () => {
   const expectedAttack = 25;
   const expectedDefence = 25;
   const expectedName = 'Bob';
-  const newClass = new classes.Bowerman('Bob', 'Bowman');
+  const newClass = new classes.Bowerman('Bob');
   const receivedAttack = newClass.attack;
   const receivedDefence = newClass.defence;
   const receivedName = newClass.name;
@@ -17,7 +17,7 @@ test('class Bowerman', () => {
 test('class Magician', () => {
   const expectedAttack = 10;
   const expectedDefence = 40;
-  const newClass = new classes.Magician('Bob', 'Magician');
+  const newClass = new classes.Magician('Bob');
   const receivedAttack = newClass.attack;
   const receivedDefence = newClass.defence;
   expect(receivedAttack).toBe(expectedAttack);
@@ -27,7 +27,7 @@ test('class Magician', () => {
 test('class Swordsman', () => {
   const expectedAttack = 40;
   const expectedDefence = 10;
-  const newClass = new classes.Swordsman('Bob', 'Swordsman');
+  const newClass = new classes.Swordsman('Bob');
   const receivedAttack = newClass.attack;
   const receivedDefence = newClass.defence;
   expect(receivedAttack).toBe(expectedAttack);
@@ -43,8 +43,38 @@ test('throws on name', () => {
   }).toThrowError(new Error('Неверный формат имени'));
 });
 
+test('class Daemon', () => {
+  const expectedAttack = 25;
+  const expectedDefence = 25;
+  const newClass = new classes.Daemon('Bob');
+  const receivedAttack = newClass.attack;
+  const receivedDefence = newClass.defence;
+  expect(receivedAttack).toBe(expectedAttack);
+  expect(receivedDefence).toBe(expectedDefence);
+});
+
+test('class Undead', () => {
+  const expectedAttack = 40;
+  const expectedDefence = 10;
+  const newClass = new classes.Undead('Bob');
+  const receivedAttack = newClass.attack;
+  const receivedDefence = newClass.defence;
+  expect(receivedAttack).toBe(expectedAttack);
+  expect(receivedDefence).toBe(expectedDefence);
+});
+
+test('class Zombie', () => {
+  const expectedAttack = 10;
+  const expectedDefence = 40;
+  const newClass = new classes.Zombie('Bob');
+  const receivedAttack = newClass.attack;
+  const receivedDefence = newClass.defence;
+  expect(receivedAttack).toBe(expectedAttack);
+  expect(receivedDefence).toBe(expectedDefence);
+});
+
 test('throws on type', () => {
   expect(() => {
-    const newClass = new classes.Swordsman('Bob', 'Sworde');
+    const newClass = new Character('Bob', 'Sworde');
   }).toThrowError(new Error('Неверный формат типа'));
 });
